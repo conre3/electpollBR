1c50f5e556093eceee13b0d6df1bba7a04f6b74b
Marcelo Ventura <mvf@usp.br>
2026-04-24 10:47:30 -0300

    creating .onLoad script

---
+++ R/init.R
@@ -0,0 +1,16 @@
+####
+# global variables in the package ----
+###
+globalVariables(c())
+
+####
+# exec on package loading ----
+###
+.onLoad <- function(lib, pkg) {
+  ## run every test in scrape.R ----
+  # TODO
+  ## check whether the structure of the download directories is ok ----
+  # TODO
+  ## if not, mount it ----
+  # TODO
+}
