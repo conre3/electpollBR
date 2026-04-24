c6e905832279d21ac39f45c04b4e57b9397cd0db
Marcelo Ventura <mvf@usp.br>
2026-04-24 10:48:09 -0300

    creating download script

---
+++ R/download.R
@@ -0,0 +1,20 @@
+####
+# download year ----
+###
+download.polls <- function() {
+  # TODO
+}
+
+####
+# unpack dowloaded files ----
+###
+unpack.polls <- function() {
+  # TODO
+}
+
+####
+# tidy up dowloaded files ----
+###
+tidyup.polls <- function() {
+  # TODO
+}
