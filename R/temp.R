7f9f463b5c67b711b55f60156e2073e820b4027c
Marcelo Ventura <mvf@usp.br>
2026-04-26 01:44:48 -0300

    improving download code

--- R/temp.R
+++ R/temp.R
@@ -22,16 +22,105 @@
 # https://dadosabertos.tse.jus.br/dataset/pesquisas-eleitorais-2026/resource/fc6b7e48-540d-4f94-a412-8829d5f022d3
 # https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/bairro_municipio_2026.zip
 
-## ZIPs de CSV ----
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_eleitoral_2026.zip", destfile = "data/zip/pesquisa_eleitoral_2026.zip", method = "curl")
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_contratante_2026.zip", destfile = "data/zip/pesquisa_contratante_2026.zip", method = "curl")
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_pagante_2026.zip", destfile = "data/zip/pesquisa_pagante_2026.zip", method = "curl")
 # download everything ----
 
-## ZIPs de PDF (mais pesados) ----
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/nota_fiscal_2026.zip", destfile = "data/zip/nota_fiscal_2026.zip", method = "curl")
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/questionario_pesquisa_2026.zip", destfile = "data/zip/questionario_pesquisa_2026.zip", method = "curl")
-download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/bairro_municipio_2026.zip", destfile = "data/zip/bairro_municipio_2026.zip", method = "curl")
+## verify whether or not the ZIP files are up-to-date ----
+### "Is this trip really necessary?"
+
+### naming stuff ----
+url.base <- "https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/"
+datasets.urls <-
+  c(
+    "pesquisa_eleitoral",
+    "pesquisa_contratante",
+    "pesquisa_pagante",
+    "nota_fiscal",
+    "questionario_pesquisa",
+    "bairro_municipio"
+  )
+datasets.names <-
+  c(
+    "poll",
+    "contractor",
+    "payer",
+    "invoice",
+    "questionnaire",
+    "detail"
+  )
+year <- 2026 |> as.character()
+zipfile.extension <- ".zip"
+zipfile.urls <-
+  paste0(url.base, datasets.urls, "_", year, zipfile.extension) |>
+  setNames(datasets.names)
+timestampfile.path <- "data/zip-timestamp.json"
+download.path.base <- "data/zip/"
+zipfiles.download.names <-
+  paste0(
+    download.path.base,
+    datasets.urls,
+    "_",
+    year,
+    zipfile.extension
+  )
+csvfiles.path <- "data/csv"
+
+### get current zip files' timestamps ----
+current <-
+  zipfile.urls |>
+  lapply(
+    function(url) {
+      httr2::request(url) |>
+        httr2::req_method("HEAD") |>
+        httr2::req_perform() |>
+        httr2::resp_headers() |>
+        purrr::pluck("Last-Modified") |>
+        lubridate::dmy_hms(tz = "")
+    }
+  ) |>
+  jsonlite::toJSON() |>  # just to get rid of the trailing "GMT" in the dates
+  jsonlite::fromJSON()
+
+### load already downloaded zip files' timestamps if any ----
+previous <-
+  if (file.exists(timestampfile.path))
+    jsonlite::read_json(timestampfile.path, simplifyVector = TRUE) else 0
+
+### check if not up-to-date ----
+zipfile.outdated <- unlist(current) != unlist(previous)
+
+#### SAFETY LOCK: 2 files are way too heavy to download them just for fun ----
+zipfile.outdated <- zipfile.outdated & c(T,T,T,T,F,F)
+zipfile.outdated <- c(T,T,T,T,F,F)
+
+### download outdated zip files if any ----
+if (any(zipfile.outdated)) {
+  zipfile.urls[zipfile.outdated] |>
+    lapply(
+      \(u)
+      httr2::request(u) |>
+        httr2::req_throttle(capacity = 10, fill_time_s = 60)
+    ) |>
+    httr2::req_perform_parallel(
+      paths = zipfiles.download.names[zipfile.outdated],
+      on_error = "continue",
+      progress = TRUE,
+      max_active = 4
+    )
+}
+
+### save current zip files' timestamps ----
+jsonlite::write_json(current, timestampfile.path, auto_unbox = TRUE)
+
+# # # initial version of download code (w/o timestamp check)
+# ## ZIPs de CSV
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_eleitoral_2026.zip", destfile = "data/zip/pesquisa_eleitoral_2026.zip", method = "curl")
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_contratante_2026.zip", destfile = "data/zip/pesquisa_contratante_2026.zip", method = "curl")
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_pagante_2026.zip", destfile = "data/zip/pesquisa_pagante_2026.zip", method = "curl")
+#
+# ## ZIPs de PDF (mais pesados)
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/nota_fiscal_2026.zip", destfile = "data/zip/nota_fiscal_2026.zip", method = "curl")
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/questionario_pesquisa_2026.zip", destfile = "data/zip/questionario_pesquisa_2026.zip", method = "curl")
+# download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/bairro_municipio_2026.zip", destfile = "data/zip/bairro_municipio_2026.zip", method = "curl")
 
 # unpack downloaded files ----
 ## CSVs first ----
