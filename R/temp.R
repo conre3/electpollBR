d506130a978a287717b0d067442a431d049e937c
Marcelo Ventura <mvf@usp.br>
2026-04-26 01:43:32 -0300

    just translating a comment

--- R/temp.R
+++ R/temp.R
@@ -22,11 +22,11 @@
 # https://dadosabertos.tse.jus.br/dataset/pesquisas-eleitorais-2026/resource/fc6b7e48-540d-4f94-a412-8829d5f022d3
 # https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/bairro_municipio_2026.zip
 
-# baixa todo mundo ----
 ## ZIPs de CSV ----
 download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_eleitoral_2026.zip", destfile = "data/zip/pesquisa_eleitoral_2026.zip", method = "curl")
 download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_contratante_2026.zip", destfile = "data/zip/pesquisa_contratante_2026.zip", method = "curl")
 download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/pesquisa_pagante_2026.zip", destfile = "data/zip/pesquisa_pagante_2026.zip", method = "curl")
+# download everything ----
 
 ## ZIPs de PDF (mais pesados) ----
 download.file("https://cdn.tse.jus.br/estatistica/sead/odsele/pesquisa_eleitoral/nota_fiscal_2026.zip", destfile = "data/zip/nota_fiscal_2026.zip", method = "curl")
